57fe178c702b18e4dcc2777594789b5f158fb9fd
Y0LRIN <chloe.arcadia.alx@gmail.com>
2026-05-11 13:09:47 +0200

    (WIP) Vehicles.cs

---
+++ Models/Vehicles.cs
@@ -0,0 +1,19 @@
+using CityDriveManager.Interfaces;
+
+namespace CityDriveManager.Models;
+
+public class Car : Vehicle
+{
+    public string Model { get; set; }
+
+    public Car(string brand, string color, string model)
+        : base(brand, color)
+    {
+        Model = model;
+    }
+
+    public override string ToString()
+    {
+        return $"[CAR]\n{base.ToString()}\nModèle : {Model}";
+    }
+}
