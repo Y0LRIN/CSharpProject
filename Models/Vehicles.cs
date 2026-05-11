c4e1a6b20e777df145b93ef4ac2e6a0da4f43b3b
Y0LRIN <chloe.arcadia.alx@gmail.com>
2026-05-11 14:32:12 +0200

    Vehicles.cs

--- Models/Vehicles.cs
+++ Models/Vehicles.cs
@@ -33,3 +33,47 @@ public class Truck : Vehicle
         return $"[TRUCK]\n{base.ToString()}\nTonnage : {Tonnage} T";
     }
 }
+
+public class HybridCar : Vehicle, IThermalCar, IElectricCar
+{
+    public double BatteryLevel { get; set; }
+    public double FuelLevel { get; set; }
+
+    public HybridCar(string brand, string color, double batteryLevel, double fuelLevel)
+        : base(brand, color)
+    {
+        BatteryLevel = batteryLevel;
+        FuelLevel = fuelLevel;
+    }
+
+    public void Recharge(double amount)
+    {
+        BatteryLevel = Math.min(100, BatteryLevel + amount);
+        Console.WriteLine($"Recharge effectuée. Batterie : {BatteryLevel}%");
+    }
+
+    public void Refuel(double amount)
+    {
+        FuelLevel += amount;
+        Console.WriteLine($"Carburant ajouté. Niveau : {FuelLevel}L");
+    }
+
+    public override void Accelerate()
+    {
+        CurrentSpeed += 10;
+
+        if (BatteryLevel > 0)
+        {
+            BatteryLevel = Math.Max(0, BatteryLevel - 2);
+        }
+        else
+        {
+            FuelLevel = Math.Max(0, FuelLevel - 1);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"[HYBRID CAR]\n{base.ToString()}\nBatterie : {BatteryLevel}%\nCarburant : {FuelLevel}L";
+    }
+}
