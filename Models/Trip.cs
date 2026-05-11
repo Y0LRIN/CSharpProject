c359c7804832bf0d8ea92512210ca05133ad19bf
Y0LRIN <chloe.arcadia.alx@gmail.com>
2026-05-11 15:57:09 +0200

    Trip.cs

---
+++ Models/Trip.cs
@@ -0,0 +1,48 @@
+namespace CityDriveManager.Models
+
+public class Trip 
+{
+    public const double AVERAGE_SPEED = 50.0;
+
+    public Vehicle Vahicle { get; set; }
+    public PointOfInterest Departure { get; set; }
+    public PointOfInterest Arrival { get; set; }
+    public DateTime DepartureDate { get; set; }
+
+    public Trip(Vehicle vehicle, PointOfInterest departure, PointOfInterest arrival, DateTime departureDate)
+    {
+      Vahicle = vehicle;
+      Departure = departure;
+      Arrival = arrival;
+      DepartureDate = departure;
+    }
+
+    public double GetDistance()
+    {
+      return Departure.CalculateDistance(Arrival);
+    }
+
+    public double GetDurationInMinutes()
+    {
+      return (GetDistance() / AVERAGE_SPEED) * 60.0;
+    }
+
+    public TimeSpan GetTimeSinceDeparture()
+    {
+      return DateTime.now - DepartureDate;
+    }
+
+    public override string ToString()
+        {
+            double distance = GetDistance();
+            int durationMin = (int)Math.Round(GetDurationInMinutes());
+
+            return $"Véhicule : {Vehicule.Brand}\n" +
+                   $"De : {Departure.Name}\n" +
+                   $"A : {Arrival.Name}\n" +
+                   $"Distance : {distance} km\n" +
+                   $"Durée estimée : {durationMin} minutes\n" +
+                   $"Départ : {DepartureDate:dd/MM/yyyy HH:mm}";
+        }
+}
+}
